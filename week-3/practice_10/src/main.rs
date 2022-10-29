ff8885f2e8895b866a35f293e5376320c4c3f9f9
olasubomi <olasubomi.oladoja@pau.edu.ng>
2022-10-29 21:43:24 +0100

    (my second commit

---
+++ week-3/practice_10/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+ // addition 
+let sum = 5550 + 7310;
+println!("the sum of 5550 and 7310 ={}", sum);
+
+//subtraction
+let difference:f32 = 95.5 - 4.3;
+println!("the difference of 95.5 and 4.3 ={}", difference);
+
+//multiplication
+let product:u32 = 4 * 30;
+println!("the multiple of 4 and 40 = {}", product);
+
+//division
+let division = 56.7 / 32.2;
+println!("the division of 56.7 and 32.2 = {}", division);
+
+//remainder 
+let remainder = 43 % 5;
+println!("the remainder of 43 and 5 ={}", remainder);
+}
---
+++ week-3/practice_5/target/debug/practice_5.d
@@ -0,0 +1,1 @@
+C:\Users\HP\Documents\o.oladojaCSC101\week-3\practice_5\target\debug\practice_5.exe: C:\Users\HP\Documents\o.oladojaCSC101\week-3\practice_5\src\main.rs
